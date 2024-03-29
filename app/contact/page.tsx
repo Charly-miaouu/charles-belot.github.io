eaf25c74e4a862a05974622d9487cab3867b9ad6
Charles Belot <charles.belot@enigma-school.com>
2024-03-29 16:38:25 +0100

    adding /contact test

---
+++ app/contact/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex-col items-center justify-between p-12 bg-gray text-black text-center">
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2">
+
+        <div className="lg:grid-rows-2">
+          <h1 className="text-2xl font-bold lg:text-left">Bonjour, je m'appelle Charles Belot,</h1><br />
+          <p className="text-lg opacity-50 text-m lg:text-left">J'ai 19 ans, je suis étudiant en informatique, j'ai des compétences en HTML, CSS, Javascript, C, Python, Java, SQL, Réseau & Cybersécurité.</p>
+          <p className="text-lg opacity-50 text-m lg:text-left"> Je parle Francais et Anglais (B2).</p>
+          <p className="text-lg opacity-50 text-m lg:text-left">Je suis passionné par l'informatique et les nouvelles technologies, je suis rigoureux , autonome et curieux.</p>
+        </div>
+
+        <div className="bg-no-repeat bg-contain bg-center bg-[url('/pdp.jpg')] relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert">
+        </div>
+      </div>
+
+    </main>
+  );
+}
