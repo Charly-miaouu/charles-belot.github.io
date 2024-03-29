5f1d89bba1bee7e2c0554fc338df882bdb361e63
Charles Belot <charles.belot@enigma-school.com>
2024-03-29 16:58:20 +0100

    improve perfomances

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
 
         <div className="lg:grid-rows-2">
           <h1 className="text-2xl font-bold lg:text-left">Bonjour, je m'appelle Charles Belot,</h1><br />
-          <p className="text-lg opacity-50 text-m lg:text-left">J'ai 19 ans, je suis étudiant en informatique, j'ai des compétences en HTML, CSS, Javascript, C, Python, Java, SQL, Réseau & Cybersécurité.</p>
-          <p className="text-lg opacity-50 text-m lg:text-left"> Je parle Francais et Anglais (B2).</p>
-          <p className="text-lg opacity-50 text-m lg:text-left">Je suis passionné par l'informatique et les nouvelles technologies, je suis rigoureux , autonome et curieux.</p>
+          <p className="text-lg text-m lg:text-left" style={{color:'#90918E'}} >J'ai 19 ans, je suis étudiant en informatique, j'ai des compétences en HTML, CSS, Javascript, C, Python, Java, SQL, Réseau & Cybersécurité.</p>
+          <p className="text-lg text-m lg:text-left" style={{color:'#90918E'}}> Je parle Francais et Anglais (B2).</p>
+          <p className="text-lg text-m lg:text-left" style={{color:'#90918E'}}>Je suis passionné par l'informatique et les nouvelles technologies, je suis rigoureux , autonome et curieux.</p>
         </div>
 
-        <div className="bg-no-repeat bg-contain bg-center bg-[url('/pdp.jpg')] relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert">
+        <div className="bg-no-repeat bg-contain bg-center bg-[url('/pdp.avif')] relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert">
         </div>
       </div>
 
