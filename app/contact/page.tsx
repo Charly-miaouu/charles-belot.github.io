37ab560503e0da89fb692104e9b5bd752fd36aff
Charles Belot <charles.belot@enigma-school.com>
2024-04-12 15:03:29 +0200

    update contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex-col items-center justify-between p-12 bg-gray text-black text-center">
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-rows-2">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0">
 
         <div className="text-lg text-m lg:text-left">
           <h1 className="text-2xl font-bold lg:text-left">Comment me contacter ?</h1><br />
@@ -12,8 +12,6 @@ export default function Home() {
           <p style={{color:'#90918E'}}>Vous pouvez aussi me contacter par téléphone au +33.7.83.88.01.92</p><br/>
           <p style={{color:'#90918E'}}>Vous pouvez aussi me contacter via ce formulaire :</p><br />
           <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScrPk8LeQfF6f_S6sihMZfxLQLd-71uRZRx0TAqe-2S2QhGFg/viewform?embedded=true" width="640" height="853">Chargement…</iframe>
-        </div>
-        <div>
         </div>
       </div>
 
